a2fadf22a393b479cc67d551b67f18797aae7b63
Arshad <arshaghnachi234@gmail.com>
2025-08-07 20:18:43 +0530

    Responsive changes

--- src/components/Others/AllTask.jsx
+++ src/components/Others/AllTask.jsx
@@ -9,12 +9,20 @@ const AllTask = () => {
       id="allTask"
       className="bg-[#1e1e2e] px-5 py-3 lg:py-5 mt-3 overflow-auto rounded-lg shadow-lg"
     >
-      <div className="bg-fuchsia-800 flex justify-between items-center mb-2 py-3 lg:px-4 rounded-lg text-white">
-        <h2 className="font-medium text-sm lg:text-lg w-1/5">Employee Name</h2>
-        <h2 className="font-medium text-sm lg:text-lg w-1/5">New Task</h2>
-        <h2 className="font-medium text-sm lg:text-lg w-1/5">Active Task</h2>
-        <h2 className="font-medium text-sm lg:text-lg w-1/5">Completed Task</h2>
-        <h2 className="font-medium text-sm lg:text-lg w-1/5 lg:w-auto">
+      <div className="bg-fuchsia-800 flex justify-between items-center mb-2 py-3 p-1 lg:px-4 rounded-lg text-white">
+        <h2 className="font-medium text-sm lg:text-lg w-1/6 lg:w-1/5">
+          Employee Name
+        </h2>
+        <h2 className="font-medium text-sm lg:text-lg w-1/6 lg:w-1/5">
+          New Task
+        </h2>
+        <h2 className="font-medium text-sm lg:text-lg w-1/6 lg:w-1/5">
+          Active Task
+        </h2>
+        <h2 className="font-medium text-sm lg:text-lg w-1/6 lg:w-1/5">
+          Completed Task
+        </h2>
+        <h2 className="font-medium text-sm lg:text-lg w-1/6 lg:1-0">
           Failed Task
         </h2>
       </div>
