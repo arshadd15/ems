e46b196af32f6285aaa5e1e8a7a3d4c50768dc93
Arshad <arshaghnachi234@gmail.com>
2025-08-07 20:28:24 +0530

    Responsive changes

--- src/components/Others/AllTask.jsx
+++ src/components/Others/AllTask.jsx
@@ -10,21 +10,15 @@ const AllTask = () => {
       className="bg-[#1e1e2e] px-5 py-3 lg:py-5 mt-3 overflow-auto rounded-lg shadow-lg"
     >
       <div className="bg-fuchsia-800 flex justify-between items-center mb-2 py-3 p-1 lg:px-4 rounded-lg text-white">
-        <h2 className="font-medium text-sm lg:text-lg w-1/4 lg:w-1/5">
-          Employee Name
-        </h2>
+        <h2 className="font-medium text-sm lg:text-lg w-1/5">Employee Name</h2>
         <h2 className="font-medium text-sm lg:text-lg w-1/8 lg:w-1/5">
           New Task
         </h2>
         <h2 className="font-medium text-sm lg:text-lg w-1/8 lg:w-1/5">
           Active Task
         </h2>
-        <h2 className="font-medium text-sm lg:text-lg w-1/4 lg:w-1/5">
-          Completed Task
-        </h2>
-        <h2 className="font-medium text-sm lg:text-lg w-1/4 lg:1-0">
-          Failed Task
-        </h2>
+        <h2 className="font-medium text-sm lg:text-lg w-1/5">Completed Task</h2>
+        <h2 className="font-medium text-sm lg:text-lg">Failed Task</h2>
       </div>
       <div>
         {userData?.employees?.map((elem, idx) => (
