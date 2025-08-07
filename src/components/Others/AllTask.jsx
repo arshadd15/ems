77b2f865d47c2738d10422ff4e19644655b70e4f
Arshad <arshaghnachi234@gmail.com>
2025-08-07 20:08:55 +0530

    Responsive changes

--- src/components/Others/AllTask.jsx
+++ src/components/Others/AllTask.jsx
@@ -10,17 +10,17 @@ const AllTask = () => {
       className="bg-[#1e1e2e] p-5 mt-3 overflow-auto rounded-lg shadow-lg"
     >
       <div className="bg-fuchsia-800 flex justify-between items-center mb-2 py-3 px-4 rounded-lg text-white">
-        <h2 className="font-medium text-md lg:text-lg w-1/5">Employee Name</h2>
-        <h2 className="font-medium text-md lg:text-lg w-1/5">New Task</h2>
-        <h2 className="font-medium text-md lg:text-lg w-1/5">Active Task</h2>
-        <h2 className="font-medium text-md lg:text-lg w-1/5">Completed Task</h2>
-        <h2 className="font-medium text-md lg:text-lg">Failed Task</h2>
+        <h2 className="font-medium text-sm lg:text-lg w-1/5">Employee Name</h2>
+        <h2 className="font-medium text-sm lg:text-lg w-1/5">New Task</h2>
+        <h2 className="font-medium text-sm lg:text-lg w-1/5">Active Task</h2>
+        <h2 className="font-medium text-sm lg:text-lg w-1/5">Completed Task</h2>
+        <h2 className="font-medium text-sm lg:text-lg">Failed Task</h2>
       </div>
       <div>
         {userData?.employees?.map((elem, idx) => (
           <div
             key={idx}
-            className="border border-orange-400 bg-white/10 flex justify-between items-center mb-2 py-3 px-4 rounded-lg transition-all duration-200 hover:scale-[103%] hover:bg-white/20 hover:shadow-md hover:shadow-orange-400/50"
+            className="border border-orange-400 bg-white/10 flex justify-between items-center mb-2 py-3 px-1 lg:px-4 rounded-lg transition-all duration-200 hover:scale-[102%] hover:bg-white/20 hover:shadow-md hover:shadow-orange-400/50"
           >
             <h2 className="text-lg font-medium w-1/5 text-white">
               {elem.firstName}
